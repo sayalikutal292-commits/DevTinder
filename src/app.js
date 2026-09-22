f3a10f9e90cbe27fa59976d5239a36f8a5bab09d
ayalikutal292-commits <sayali.kutal292@gmail.com>
2026-09-22 19:55:38 +0530

    Get, post methods

--- src/app.js
+++ src/app.js
@@ -4,13 +4,37 @@ const app = express();
 const connectDB = require("./Config/database.js");
 const User = require("./models/user.js");
 
-app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Raavi",
-    lastName: "Kutal",
-    emailId: "Ravvi",
-  });
+app.use(express.json());
 
+// get all user data
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Error while reding data" + err.message);
+  }
+});
+
+// get one user
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.findOne({ emailId: req.body.emailId });
+    console.log(user);
+    if (user === null) {
+      res.send("User not found");
+    } else {
+      res.send(user);
+    }
+  } catch (err) {
+    res.status(400).send("Something went wrong!!");
+  }
+});
+
+app.post("/signup", async (req, res) => {
+  // creating a new instance of the user model
+  const user = new User(req.body);
   try {
     await user.save();
     res.send("User added succesfully!!");
